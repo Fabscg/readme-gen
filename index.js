ba4b12ada0b5a2e0fec8ad34328e369d958ca10a
Fabulosa <fabiscg79@gmail.com>
2021-12-10 22:19:06 -0500

    Added some functions and console.log to see if by far the coding is working

--- index.js
+++ index.js
@@ -2,13 +2,96 @@ const inquirer = require('inquirer')
 const fs = require('fs')
 const markdown = require('./utils/generateMarkdown')
 
-inquirer.prompt([
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?'
-    },
-])
-.then(function(data) {
-    console.log(data)
-})
+
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?(Required)'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project(Required)',
+            validate: description => {
+                if (description) {
+                    return true;
+                } else {
+                    console.log('You need to enter a description');
+                    return false
+                }
+            }
+        },
+        {
+            type: 'header',
+            name: 'tableOfcontents',
+            message: 'Table of Contents(Optional)'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What dependencies were installed in this project?(Required)',
+            validate: installation => {
+                if (installation) {
+                    return true
+                        ;
+                } else {
+                    return false
+                }
+
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide instructions and examples for use. Include screenshots as needed.'
+        },
+        {
+            type: 'input',
+            name: 'Credits',
+            message: 'List your collaborators if any'
+        },
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: 'What license protect this project?(Check the one that apply)',
+            choices: ['MIT', 'GNU', 'Mozilla Public License', 'Apache', 'Boost Soaftware', 'none']
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Test'
+        },
+        {
+            input: 'input',
+            name: 'questions',
+            message: "If you have any questions about the project, you can reach me here:",
+        },
+
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?(Required)'
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'What is your Github username?(Required)'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Provide your email address'
+        }
+    ])
+    .then((data) => {
+    const readmeData = markdown.generateMarkdown(data)
+    fs.writeFile('generateMarkdown', readmeData, function (err) {
+        if (err) throw err;
+        console.log('Data Saved!');
+    })
+});
+
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,20 @@
+
+
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+   ${data.description}
+
+  ## Table of Contents
+  ${data.header}
+
+  ## Installation
+  ${data.installation}
+
 `;
 }
 
-module.exports = generateMarkdown;
+
+module.exports = generateMarkdown (data);
